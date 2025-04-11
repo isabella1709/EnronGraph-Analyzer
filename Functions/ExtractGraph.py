681a3767d336b4768f1c7be78f113fe58cde3819
Isabella Vanderlinde Berkembrock <130788816+isabella1709@users.noreply.github.com>
2025-04-11 16:11:51 -0300

    Update ExtractGraph.py

--- Functions/ExtractGraph.py
+++ Functions/ExtractGraph.py
@@ -1,12 +1,12 @@
 from collections import defaultdict
 
 def getOrder(graph):
-    return graph.order
+    return graph.order # Retorna o número de vértices do grafo
 
 def getSize(graph):
-    return graph.size
+    return graph.size # Retorna o número de arestas do grafo
 
-def getIsolatedVertices(graph):
+def getIsolatedVertices(graph): # Retorna vétices sem grau de saída ou entrada
     count = 0
     for key in graph.graph:
         if (graph.degree(key) == 0):
@@ -14,29 +14,29 @@ def getIsolatedVertices(graph):
     
     return count
 
-def getMaximumOutDegrees(graph):
+def getMaximumOutDegrees(graph): 
     vertices = []
 
     for key in graph.graph:
         outDegree = graph.out_degree(key)
         vertices.append((key, outDegree))
 
-    sortedVertices = sorted(vertices, key=lambda x: x[1], reverse=True)
+    sortedVertices = sorted(vertices, key=lambda x: x[1], reverse=True) # Ordena a lista de tuplas a partir do grau
 
-    return sortedVertices[:20]
+    return sortedVertices[:20] # Retorna os 20 vértices com maior grau de saída
 
 
 def getMaximumEnterDegrees(graph):
     enter_degrees = defaultdict(int)
 
-    for u in graph.graph:
+    for u in graph.graph: # Conta o grau de entrada de todos os nós
         for v, _ in graph.graph[u]:
             enter_degrees[v] += 1
 
-    for v in graph.graph:
+    for v in graph.graph: # Garante que todos os nós estejam no dicionário 
         if v not in enter_degrees:
             enter_degrees[v] = 0
 
-    sortedVertices = sorted(enter_degrees.items(), key=lambda x: x[1], reverse=True)
+    sortedVertices = sorted(enter_degrees.items(), key=lambda x: x[1], reverse=True) # Ordena os nós pelo grau de entrada
 
-    return sortedVertices[:20]
+    return sortedVertices[:20] # Retorna os 20 vértices com maior grau de entrada
